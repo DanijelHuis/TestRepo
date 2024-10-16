9b305c5a4488ea45aa4dad7748b247c10cc56d1c
Danijel Huis <danijel.huis.app@gmail.com>
2024-10-16 10:34:12 +0200

    Fastlane package update for release 0.0.6.11

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,24 +17,18 @@ let package = Package(
         .package(url: "https://github.com/gematik/OpenSSL-Swift", from: "4.0.0"),
     ],
     targets: [
-        // Docs: https://docs.swift.org/package-manager/PackageDescription/PackageDescription.html#target
         .binaryTarget(
             name: "CardLinkSDK",
-            url: "https://github.com/DanijelHuis/TestRepo/releases/download/0.0.1/CardLinkSDK.xcframework.zip", 
-            checksum: "1c6a58a0a6e1768d75560627dbb28fbf22a868145217b1d98becdd3c9652c7ef"
-            //dependencies: [...]     // Nope, binaryTarget cannot have dependenices for some reason.
+            url: "https://github.com/DanijelHuis/TestRepo/releases/download/0.0.6/CardLinkSDK.xcframework.zip", 
+            checksum: "364566e773d74869f3f8535bc7fb4c2e7d9f00cc0f57b86cc9c5df10a53808a0"
         ),
-        // binaryTarget doesn't support dependencies. That means that we cannot specify that WeatherSDK depends on WeatherCore.
-        // The workaround is to create wrapper target (WeatherSDKTargets) and specify WeatherCore and WeatherSDK as its dependencies. That way Xcode will pull both.
-        // Note that this wrapper target is not actual target in Xcode, it doesn't exist except here in package.
-        // In order for this to work I had to add Sources/WeatherSDK/Empty.swift.
         .target(
             name: "CardLinkSDKWrapper",
             dependencies: [ 
                 .target(name: "CardLinkSDK"),
                 .product(name: "OpenSSL-Swift", package: "OpenSSL-Swift"),
             ],
-            path: "Sources/CardLinkSDKWrapper"  // This must exist and it must have some empty source file.
+            path: "Sources/CardLinkSDKWrapper"
         )
     ]
 )
